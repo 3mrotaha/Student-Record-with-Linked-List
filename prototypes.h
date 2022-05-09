091fd3e7451e35a4473cc93ab18f0c25ba04b2dc
amr12601 <62963089+amr12601@users.noreply.github.com>
2022-05-09 02:09:52 +0200

    Functions Prototypes

---
+++ prototypes.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "std_types.h"
+
+void Load_Database(void);
+
+void freeData(void);
+
+f32 get_AvrGrades(int weeks, int days, f32 Grades[weeks][days]);
+
+void GPA(f32 grade);
+
+void display_Student(int idNum);
+
+void DeleteStudent(int idNum);
+
+void AddnewStudent(void);
+
+void DisplyDatabase(void);
+
